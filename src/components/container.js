00e793b01904a7d17330cb9ee36fcf415d8e34e2
Cristian C. Castillo <cris.c.castillo1@gmail.com>
2020-08-19 19:33:31 -0700

    Added Markdown features
    You may now type into the editor and it will display into the preview section box

--- src/components/container.js
+++ src/components/container.js
@@ -1,18 +1,56 @@
 import React, { Component } from 'react'
+
 import './containerStyle.css'
-import MiniBox from './miniBox'
-import BigBox from './bigBox'
+import './miniBoxStyle.css'
+import './bigBoxStyle.css'
+
+import {SiWindows95} from "react-icons/si"
+import Badge from "react-bootstrap/Badge";
+
+let marked = require("marked");
+
+
 
 export default class container extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            markdown: "",
+        };
+    }
 
+    updateMarkdown(markdown) {
+        this.setState({ markdown });
+    }
 
-    render() {
-        return (
-            <div style ={{marginTop:'15px',marginLeft:'8px',marginRight:'8px',marginBottom:'60px'}}>
-                <MiniBox />
-                <BigBox />
+    render() {return (
+<div style ={{marginTop:'15px',marginLeft:'8px',marginRight:'8px',marginBottom:'60px'}}>
+    <div className = 'container'>
+        <div className = 'header'><h5><span><SiWindows95 className ='windows' /> Editor</span></h5></div>
+            <div className = 'body'>
+           
+                    <textarea
+                    value={this.state.markdown}
+                    className = 'content'
+                    onChange={(e) => {
+                        this.updateMarkdown(e.target.value)
+                    }}
+                    ></textarea>
                 
             </div>
+        </div>
+        <div className = 'containerBigBox'>
+            <div className = 'headerBigBox'><h5><span><SiWindows95 className ='windows' /> Previewer</span></h5></div>
+                <div className = 'bodyBigBox'>
+                    <div className = 'contentBigBox'>
+                        <div dangerouslySetInnerHTML={{
+                                __html: marked(this.state.markdown),
+                        }}>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         )
     }
 }
